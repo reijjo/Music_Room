d6a31ed9abf06fb862fabd3b14c7d460b5e8e8af
reijjo <teemu.aitomeri@gmail.com>
2023-10-25 21:30:07 +0300

    login page started

--- web/client/src/pages/public/Login.tsx
+++ web/client/src/pages/public/Login.tsx
@@ -1,9 +1,41 @@
+import MyButton from "../../components/MyButton";
+import MyInput from "../../components/MyInput";
+
 const Login = () => {
   return (
     <div className="login-page">
-      <h1>jeee login</h1>
-      <input />
-      <input />
+      <h1 style={{ fontFamily: "MilgranRegular" }}>Log in</h1>
+      <form style={{ width: "100%" }}>
+        <div className="reg-fields">
+          <div className="input-block">
+            <div className="label-input">
+              <label htmlFor="user">Email / Username</label>
+              <MyInput className="my-input" />
+            </div>
+          </div>
+          <div className="input-block">
+            <div className="label-input">
+              <label htmlFor="user">Password</label>
+              <MyInput className="my-input" />
+            </div>
+          </div>
+          <div className="input-block">
+            <div className="label-input">
+              <MyButton
+                className="my-button filled-button"
+                style={{
+                  // width: "50%",
+                  padding: "0.5rem 1rem",
+                  fontSize: "1rem",
+                }}
+                type="submit"
+              >
+                Log In!
+              </MyButton>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
